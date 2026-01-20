0d2919cb3f04ca8aa991ae0cbc84fe026ed26ac7
DipuYadav0917 <ydvdiyan2004@gmail.com>
2026-01-20 21:34:42 +0530

    Fix axios baseURL using VITE_API_URL

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,10 @@
+
 import { createContext, useContext, useEffect, useState } from "react";
 import axios from 'axios'
 import {toast} from 'react-hot-toast'
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 export const AppContext = createContext();
 
